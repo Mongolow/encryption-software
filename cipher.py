ca74384611fa295a6e9b50cc35b1ddc19928dff2
Michal Gniadek <mongolow0072005@gmail.com>
2025-12-31 13:34:25 +0100

    program class optimalization

--- cipher.py
+++ cipher.py
@@ -1,35 +1,54 @@
 import zlib
 import secrets
-class Cipher:
-    def __init__(self):
-        pass
-    def encrypt(self,filename):
-        #file bytes read 
-        with open(filename,'rb') as file:
-            file_content = file.read()
-        #file compress
-        file_content = zlib.compress(file_content)
-        #key generation
-        key = []
-        for x in range(0,20):
-            key.append(secrets.randbelow(255) + 1)
-        #file encrypt
-        counter = 0
-        encrypted = bytearray()
-        for char in file_content:
+def open_file(filename):
+    with open(filename,'rb') as file:
+        file_content = file.read()
+    return file_content
+def write_file(filename,content):
+    with open(filename,'wb') as file:
+        file.write(content)
+def encrypt_decrypt_bytearray(data, key, mode):
+    counter = 0
+    result = bytearray()
+    for char in data:
+        if mode == 'encrypt':
             if counter % 2 == 0:
                 char = char + key[counter]
             else:
                 char = char - key[counter]
                 if char < 0:
                     char = char + 256
-            encrypted.append(char % 256)
-            counter += 1
-            if counter > len(key) - 1:
-                counter = 0
+        elif mode == 'decrypt':
+            if counter % 2 == 0:
+                char = char - key[counter]
+                if char < 0:
+                    char = char + 256
+            else:
+                char = char + key[counter]
+        result.append(char % 256)
+        counter += 1
+        if counter > len(key) - 1:
+            counter = 0
+    return result
+def key_generation():
+    key = []
+    for x in range(0,20):
+        key.append(secrets.randbelow(255) + 1)
+    return key
+class Cipher:
+    def __init__(self):
+        pass
+    def encrypt(self,filename):
+        #file bytes read 
+        file_content = open_file(filename)
+        #file compress
+        file_content = zlib.compress(file_content)
+        #key generation
+        key = key_generation()
+        #file encrypt
+        encrypted = encrypt_decrypt_bytearray(file_content, key, 'encrypt')
         #changing file
-        with open(filename,'wb') as file:
-            file.write(encrypted)
+        write_file(filename,encrypted)
         #massage
         print('\nfile encrypted')
         key_ = ''
@@ -39,59 +58,32 @@ class Cipher:
         return key_[1:len(key_)]
     def decrypt(self,filename,key):
         #file read
-        with open(filename,'rb') as file:
-            file_content = file.read()
+        file_content = open_file(filename)
         #key read
         key = str(key).split(sep= '-')
+        key_ = []
+        for x in key:
+            key_.append(int(x))
         #file decrypt
-        counter = 0
-        decrypted = bytearray()
-        for char in file_content:
-            if counter % 2 == 0:
-                char = char - int(key[counter])
-                if char < 0:
-                    char = char + 256
-            else:
-                char = char + int(key[counter])
-            decrypted.append(char % 256)
-            counter += 1
-            if counter > len(key) - 1:
-                counter = 0
+        decrypted = encrypt_decrypt_bytearray(file_content, key_, 'decrypt')
         #file decompress
         decrypted = zlib.decompress(decrypted)
         #changing file
-        with open(filename,'wb') as file:
-            file.write(decrypted)
+        write_file(filename,decrypted)
+        #massage
         print('\nfile decrypted')
     def encrypt_with_copy(self,filename):
-        with open(filename,'rb') as file:
-            file_content = file.read()
+        file_content = open_file(filename)
         #making backup copy
-        with open(f'backup_{filename}','wb') as file:
-            file.write(file_content)
+        write_file(f'copy_of_{filename}',file_content)
         #file compress
         file_content = zlib.compress(file_content)
         #key generation
-        key = []
-        for x in range(0,20):
-            key.append(secrets.randbelow(255) + 1)
+        key = key_generation()
         #file encrypt
-        counter = 0
-        encrypted = bytearray()
-        for char in file_content:
-            if counter % 2 == 0:
-                char = char + key[counter]
-            else:
-                char = char - key[counter]
-                if char < 0:
-                    char = char + 256
-            encrypted.append(char % 256)
-            counter += 1
-            if counter > len(key) - 1:
-                counter = 0
+        encrypted = encrypt_decrypt_bytearray(file_content, key, 'encrypt')
         #changing file
-        with open(filename,'wb') as file:
-            file.write(encrypted)
+        write_file(filename,encrypted)
         #massage
         print('\nfile encrypted')
         key_ = ''
